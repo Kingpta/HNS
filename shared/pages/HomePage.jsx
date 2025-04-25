283693139c8fa363a10dbc941950bcb0543ecf46
Adeniji odunola <kingpeteradeniji1@gmail.com>
2025-04-24 17:05:09 -0700

    Initial commit

---
+++ shared/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
